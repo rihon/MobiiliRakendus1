0eebe1e8ad31d199e585fb2308a7550cda302f73
Riho Noormets <31840233+rihon@users.noreply.github.com>
2019-03-16 16:23:17 +0200

    Game finished

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button[][] buttons = new Button[3][3];
+
     private boolean player1Turn = true;
+
     private int roundCount;
 
     private int player1Points;
@@ -33,8 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 buttons[i][j] = findViewById(resID);
                 buttons[i][j].setOnClickListener(this);
             }
-
         }
+
         Button buttonReset = findViewById(R.id.button_reset);
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,70 +52,101 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (!((Button) v).getText().toString().equals("")) {
             return;
         }
-        if(player1Turn){
+
+        if (player1Turn) {
             ((Button) v).setText("X");
-        }else{
+        } else {
             ((Button) v).setText("O");
         }
+
         roundCount++;
 
-        if(checkForWin()){
-            if(player1Turn){
+        if (checkForWin()) {
+            if (player1Turn) {
                 player1Wins();
-            }else{
+            } else {
                 player2Wins();
             }
-        }else if(roundCount ==9){
+        } else if (roundCount == 9) {
             draw();
-        }else{
-            player1Turn=!player1Turn;
+        } else {
+            player1Turn = !player1Turn;
         }
 
     }
 
-    private boolean checkForWin(){
+    private boolean checkForWin() {
         String[][] field = new String[3][3];
 
-        for(int i=0;i<3;i++){
-           for(int j=0;j<3;j++){
-               field[i][j] =buttons[i][j].getText().toString();
-           }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
         }
-        for (int i=0;i<3;i++){
-            if(field[i][0].equals(field[i][1])
-                && field[i][0].equals(field[i][2])
-                && !field[i][0].equals("")){
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
                 return true;
             }
         }
-        for (int i=0;i<3;i++){
-            if(field[0][i].equals(field[1][i])
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
                     && field[0][i].equals(field[2][i])
-                    && !field[0][i].equals("")){
+                    && !field[0][i].equals("")) {
                 return true;
             }
         }
-        if(field[0][0].equals(field[1][1])
+
+        if (field[0][0].equals(field[1][1])
                 && field[0][0].equals(field[2][2])
-                && !field[0][0].equals("")){
+                && !field[0][0].equals("")) {
             return true;
         }
-        if(field[0][2].equals(field[1][1])
+
+        if (field[0][2].equals(field[1][1])
                 && field[0][2].equals(field[2][0])
-                && !field[0][2].equals("")){
+                && !field[0][2].equals("")) {
             return true;
         }
+
         return false;
-
-
     }
-    private void player1Wins(){
 
+    private void player1Wins() {
+        player1Points++;
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
     }
-    private void player2Wins(){
 
+    private void player2Wins() {
+        player2Points++;
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
     }
-    private void draw(){
-        
+
+    private void draw() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+        resetBoard();
+    }
+
+    private void updatePointsText() {
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+    }
+
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
     }
 }
